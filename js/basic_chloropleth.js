d7d11ceeca86117d08d268d78e2145636554b355
tintintje <christine.vinck@gmail.com>
2017-11-30 19:28:28 +0100

    chloropleth is updated by slider

--- js/basic_chloropleth.js
+++ js/basic_chloropleth.js
@@ -1,3 +1,5 @@
+// NOTE: By declaring a variable without 'var', makes it a global variable
+
 //--- Map
 var map = d3.geomap.choropleth()
     .geofile('d3-geomap/topojson/world/countries.json')
@@ -17,36 +19,21 @@ d3.csv('data/vaccinaties.csv', function(error, data) {
     var years = getYears();
 
     // call select func to create dropdown and add labels
-    var select_vaccine = d3.select('#dropdown-vaccine')
+    var select = d3.select('#dropdown')
       .append('select')
       	.attr('class','select')
         .attr('id','vaccine-select')
         .on('change', onchange);
 
-    var select_year = d3.select('#dropdown-year')
-      .append('select')
-      	.attr('class','select')
-        .attr('id','year-select')
-        .on('change', onchange);
-
     //  set selction options
-    var options_vaccine = select_vaccine
+    var options = select
       .selectAll('option')
     	.data(vaccines).enter()
     	.append('option')
     	.text(function (d) { return d; });
 
-    var options_year = select_year
-      .selectAll('option')
-    	.data(years).enter()
-    	.append('option')
-        .attr('class','option')
-        .attr('id', function(d) { return d; })
-    	.text(function (d) { return d; });
-
     // get selections
-    selected_vaccine = d3.select('#vaccine-select').property('value')
-    selected_year = d3.select('#year-select').property('value')
+    var selected_vaccine = d3.select('#vaccine-select').property('value')
 
     // get data for selected Vaccine and update selected year
     var data_vaccine = getData(selected_vaccine);
@@ -55,7 +42,9 @@ d3.csv('data/vaccinaties.csv', function(error, data) {
     d3.select('#map')
         .datum(data_vaccine)
         .call(map.draw, map);
-    // map.column(selected_year).update()
+
+    // add Slider
+    addSlider(data);
 });
 
 function getData(selected) {
@@ -82,7 +71,7 @@ function getYears() {
   var years = [];
   var headers = Object.keys(data[0]);
   headers.forEach(function(h) {
-    if(!isNaN(h)) {
+    if(h && !isNaN(h) ) {
       years.push(h);
     }
   });
@@ -91,13 +80,9 @@ function getYears() {
 
 function onchange() {
   // get selected value
-	selected_vaccine = d3.select('#vaccine-select').property('value');
-  selected_year = d3.select('#year-select').property('value');
-
+	var selected_vaccine = d3.select('#vaccine-select').property('value');
   // set new map data
   map.data = getData(selected_vaccine);
-  map.column(selected_year).update();
-
   // update map
   map.update()
 };
@@ -105,55 +90,70 @@ function onchange() {
 
 
 // ---- Slider ----
-var svg = d3.select("svg"),
-    margin = {right: 10, left: 10},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height");
+function addSlider(data) {
+  svg = d3.select("svg"),
+  margin = {right: 15, left: 15},
+  width = +svg.attr("width") - margin.left - margin.right,
+  height = +svg.attr("height");
 
-var x = d3.scaleLinear()
-    .domain([0, 180])
-    .range([0, width])
-    .clamp(true);
+  years = getYears();
 
-var slider = svg.append("g")
-    .attr("class", "slider")
-    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+  x = d3.scaleLinear()
+      .domain([0, years.length - 1])
+      .range([0, width])
+      .clamp(true);
 
-slider.append("line")
-    .attr("class", "track")
-    .attr("x1", x.range()[0])
-    .attr("x2", x.range()[1])
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-inset")
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-        .on("start.interrupt", function() { slider.interrupt(); })
-        .on("start drag", function() { hue(x.invert(d3.event.x)); })
-      );
+  var slider = svg.append("g")
+      .attr("class", "slider")
+      .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
 
-slider.insert("g", ".track-overlay")
-    .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
-  .selectAll("text")
-  .data(x.ticks(10))
-  .enter().append("text")
-    .attr("x", x)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d + "°"; });
+  slider.append("line")
+      .attr("class", "track")
+      .attr("x1", x.range()[0])
+      .attr("x2", x.range()[1])
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .attr("class", "track-inset")
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .attr("class", "track-overlay")
+      .call(d3.drag()
+          .on("start.interrupt", function() { slider.interrupt(); })
+          .on("start drag", function() { updateMap(x.invert(d3.event.x)); })
+        );
 
-var handle = slider.insert("circle", ".track-overlay")
-    .attr("class", "handle")
-    .attr("r", 9);
+  slider.insert("g", ".track-overlay")
+      .attr("class", "ticks")
+      .attr("transform", "translate(0," + 20 + ")")
+    .selectAll("text")
+    .data(x.ticks(years.length))
+    .enter().append("text")
+      .attr("x", x)
+      .attr("text-anchor", "middle")
+      .text(function(d) { return years[d]; });
 
-slider.transition() // Gratuitous intro!
-  .duration(750)
-  .tween("hue", function() {
-    var i = d3.interpolate(0, 70);
-    return function(t) { hue(i(t)); };
-});
+  handle = slider.insert("circle", ".track-overlay")
+      .attr("class", "handle")
+      .attr("r", 9);
 
-function hue(h) {
+  slider.transition() // Gratuitous intro!
+    .duration(750)
+    .tween("updateMap", function() {
+      var i = d3.interpolate(0, 25);
+      return function(t) { updateMap(i(t)); };
+  });
+
+  console.log('# years: ' + years.length)
+};
+
+function updateMap(h) {
+  // adjust slider position
   handle.attr("cx", x(h));
+  // get year from slider value
+  var slider_value = Math.round(h);
+  var slider_year = years[slider_value];
+  console.log(h)
+  console.log(slider_value)
+  map.column(slider_year).update();
+
+  // (tijdelijk) just cause it's pretty
   svg.style("background-color", d3.hsl(h, 0.8, 0.8));
 }
