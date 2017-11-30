97ab24a36187004cfdf628effd1567440e29f731
tintintje <christine.vinck@gmail.com>
2017-11-30 16:56:10 +0100

    dropdown by vaccine and year

--- js/basic_chloropleth.js
+++ js/basic_chloropleth.js
@@ -8,31 +8,55 @@ var map = d3.geomap.choropleth()
 
 
 d3.csv('data/vaccinaties.csv', function(error, data) {
+    // set data to global
     this.data = data;
+
     // get unique vaccine names
-    var labels = getLabels();
+    var vaccines = getLabels();
+    // get years
+    var years = getYears();
 
     // call select func to create dropdown and add labels
-    var select = d3.select('#dropdown')
+    var select_vaccine = d3.select('#dropdown-vaccine')
       .append('select')
       	.attr('class','select')
+        .attr('id','vaccine-select')
         .on('change', onchange);
 
-    var options = select
+    var select_year = d3.select('#dropdown-year')
+      .append('select')
+      	.attr('class','select')
+        .attr('id','year-select')
+        .on('change', onchange);
+
+    //  set selction options
+    var options_vaccine = select_vaccine
       .selectAll('option')
-    	.data(labels).enter()
+    	.data(vaccines).enter()
     	.append('option')
     	.text(function (d) { return d; });
 
-    // get selection
-    selected = d3.select('select').property('value')
+    var options_year = select_year
+      .selectAll('option')
+    	.data(years).enter()
+    	.append('option')
+        .attr('class','option')
+        .attr('id', function(d) { return d; })
+    	.text(function (d) { return d; });
 
-    // get data for selected Vaccine
-    var data_vaccine = getData(selected);
+    // get selections
+    selected_vaccine = d3.select('#vaccine-select').property('value')
+    selected_year = d3.select('#year-select').property('value')
+
+    // get data for selected Vaccine and update selected year
+    var data_vaccine = getData(selected_vaccine);
+
     // add data to map and draw
     d3.select('#map')
         .datum(data_vaccine)
         .call(map.draw, map);
+
+    // map.column(selected_year).update()
 });
 
 function getData(selected) {
@@ -53,17 +77,27 @@ function getLabels() {
     }
   });
   return uniqueNames;
-}
+};
+
+function getYears() {
+  var years = [];
+  var headers = Object.keys(data[0]);
+  headers.forEach(function(h) {
+    if(!isNaN(h)) {
+      years.push(h);
+    }
+  });
+  return years;
+};
 
 function onchange() {
   // get selected value
-	selected = d3.select('select').property('value');
+	selected_vaccine = d3.select('#vaccine-select').property('value');
+  selected_year = d3.select('#year-select').property('value');
 
-  // get data for selected Vaccine
-  var data_vaccine = getData(selected);
-
-  // set map data to selected
-  map.data = data_vaccine;
+  // set new map data
+  map.data = getData(selected_vaccine);
+  map.column(selected_year).update();
 
   // update map
   map.update()
