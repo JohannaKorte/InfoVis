7c6d2e1854847c1b6f0cf02b24ad76e9b13ddde5
tintintje <christine.vinck@gmail.com>
2017-12-16 23:14:28 +0100

    adjusted index.html layout

--- js/hbarV4.js
+++ js/hbarV4.js
@@ -1,4 +1,4 @@
-var svg = d3.select("svg"),
+var svg = d3.select("#hbar-svg"),
   margin = {top: 20, right: 20, bottom: 30, left: 80},
   width = +svg.attr("width") - margin.left - margin.right,
   height = +svg.attr("height") - margin.top - margin.bottom;
